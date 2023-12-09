974204f3d8b7ce97e5188c11f83b7a1ec1122169
Akshita Agrawal <akshitaagrawal2468@gmail.com>
2023-12-09 14:33:10 +0530

    Add code of main frame

---
+++ src/redux/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import invoicesReducer from "./invoicesSlice"; 
+// Import your other reducers
+
+const rootReducer = combineReducers({
+  invoices: invoicesReducer,
+});
+
+export default rootReducer;
